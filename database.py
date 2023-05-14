aa38ece92fd61301eca358e5208c3f9b14df3ebb
SeongjoRa <95401647+SeongjoRa@users.noreply.github.com>
2023-05-14 21:53:35 +0800

    Add files via upload

---
+++ database.py
@@ -0,0 +1,16 @@
+from deta import Deta
+
+deta_key = "b0ihc9w1kgk_h2piYahYtYynJ3e44uP5xLqNS3Tvy9oE"
+
+deta = Deta(deta_key)
+db = deta.Base("your_form")
+
+def save_data(name, keywords, img):
+    return db.put({"key":name,"keywords":keywords, "image":img})
+
+def fetch_all():
+    res = db.fetch()
+    return res.items
+
+def get_product(product_key):
+    return db.get(product_key)
